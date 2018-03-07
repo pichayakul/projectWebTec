85f5c60a2fb5cb10b92168dff8abd0704864391b
suphawich <suphawich.s@ku.th>
2018-03-07 22:16:10 +0700

    เพิ่มไฟล์
    managementuser
    management
    แก้ไข database

---
+++ management.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Management</title>
+	<meta charset="utf-8">
+</head>
+<?php require './header.php';?>
+<?php
+	require './database/epmtfafn_satta_db.php';
+	$db = new Database();
+	$db->openDatabase();
+	$account = $db->get_account_all();
+	$total_account = count($account);
+	$active_account = 0;
+	$ban_account = 0;
+	foreach ($account as $user) {
+		foreach ($user as $key => $value) {
+			if(!strcmp($key, "status_ban")) {
+				if ($value == 0) {
+					$active_account++;
+				} else {
+					$ban_account++;
+				}
+			}
+		}
+	}
+	$events = $db->get_event_and_seminar_all();
+	$total_event = count($events);
+	$event = 0;
+	$seminar = 0;
+	$fevent = 0;
+	$fseminar = 0;
+	foreach ($events as $arr) {
+		if ($arr['type'] == "event") {
+			$event++;
+			if ($arr['status'] == 1) {
+				$fevent++;
+			}
+		} else {
+			$seminar++;
+			if ($arr['status'] == 1) {
+				$fseminar++;
+			}
+		}
+	}
+	$db->closeDatabase(); 
+?>
+<body>
+	<div class="row content">
+		<div class="col-sm-1" style="background-color:lavender;"></div>
+		<div class="col-sm-10">
+			<div class="row">
+				<div class="col-sm-12 clearfix">
+					<br>
+					<a class="btn btn-primary btn-sm float-right" href="./manageuser.php">Management User</a>
+					<a class="btn btn-primary btn-sm float-right" href="./eventreport.php">Report</a>
+					<br>
+					<hr>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12 text-center">
+					<h2>Website Detail</h2>
+					<br>
+					<hr>
+					<br>
+				</div>
+			</div>
+			<div class="row">
+				<h4>website</h4>
+				<table class="table table-bordered table-striped">
+					<tbody>
+						<tr>
+							<td class="w-25">Website name</td>
+							<td>Satta Garden</td>
+						</tr>
+						<tr>
+							<td>About</td>
+							<td>Management event and seminar.</td>
+						</tr>
+						<tr>
+							<td>Contact</td>
+							<td>suphawich.s@ku.th</td>
+						</tr>
+					</tbody>
+				</table>
+				<h4>User</h4>
+				<table class="table table-bordered table-striped">
+					<tbody>
+						<tr>
+							<td class="w-25">Total account</td>
+							<td><?php echo $total_account;?> users</td>
+						</tr>
+						<tr>
+							<td>Active account</td>
+							<td><?php echo $active_account;?> users</td>
+						</tr>
+						<tr>
+							<td>Banned account</td>
+							<td><?php echo $ban_account;?> users</td>
+						</tr>
+					</tbody>
+				</table>
+				<h4>Event & Seminar</h4>
+				<table class="table table-bordered table-striped">
+					<tbody>
+						<tr>
+							<td class="w-25">Total room</td>
+							<td><?php echo $total_event;?> rooms</td>
+						</tr>
+						<tr>
+							<td>Total Event</td>
+							<td><?php echo $event;?> rooms</td>
+						</tr>
+						<tr>
+							<td>Total Seminar</td>
+							<td><?php echo $seminar;?> rooms</td>
+						</tr>
+						<tr>
+							<td>Finished event</td>
+							<td><?php echo $fevent;?> rooms</td>
+						</tr>
+						<tr>
+							<td>Finished seminar</td>
+							<td><?php echo $fseminar;?> rooms</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="col-sm-1" style="background-color:lavender;"></div>
+	</div>
+</body>
+</html>
